c7e34e7df89fb480ac74286a8ee1af56336aa46c
rahmafida <rahma.fida@gmail.com>
2015-03-08 16:40:31 +0700

    update si direktur

--- application/views/direktur/v_report_transaksigas.php
+++ application/views/direktur/v_report_transaksigas.php
@@ -34,7 +34,7 @@
                               else { ?>
                                 <?php foreach ($hasil as $transaksigasonline) {?>
                                   <tr>
-                                  <td><?php echo $transaksigasonline->idTransaksi ?></td>
+                                  <td><?php echo $transaksigasonline->idTransaksi_Online ?></td>
                                   <td><?php echo $transaksigasonline->tanggalTransaksiOnline ?></td>
                                   <td><?php echo $transaksigasonline->idPangkalan ?></td>
                                   <td><?php echo $transaksigasonline->jumlahGas ?></td>
@@ -46,6 +46,24 @@
                             </tr>
                           </tbody>
                           </table> 
+
+                          <table class="table table-striped ">
+                                        <thead>
+                                            <tr>
+                                                <th>Total :</th>
+                                                <td><?php if(empty($hasil)) {
+                                                echo "Data transaksi gas masih kosong";
+                                                }
+                                                else { ?>
+                                                  <?php foreach ($hasil as $transaksigasonline) {?>
+                                                    <tr>
+                                                    <td><?php echo $transaksigasonline->idTransaksi_Online ?></td>
+                                               <?php } ?>
+                                               <?php } ?></td>
+                                                              </tr>
+                                                          </thead>
+                          </table>
+
                               
                        <!-- <div class="form-group">
                               <div class="col-lg-10 col-lg-offset-1">
@@ -63,19 +81,6 @@
                     <fieldset>
                         <legend><center>Laporan Transaksi Gas Offline</center></legend>
                     <!-- Button trigger modal -->
-                    <div class="form-group">
-                      
-            
-                        <div class="col-lg-4 form-group">
-                        <a href="#" data-toggle="modal" data-target=".bs-example-modal-lg">
-                        Create New
-                         </a>
-                        
-                        </div>
-
-              <!-- Modal -->
-
-                      </div>
 
                         <table class="table table-striped table-hover ">
                               <thead>
@@ -97,8 +102,8 @@
                               else { ?>
                                 <?php foreach ($hasil as $transaksigasonline) {?>
                                   <tr>
-                                  <td><?php echo $transaksigasonline->idTransaksi ?></td>
-                                  <td><?php echo $transaksigasonline->tanggalTransaksiOnline ?></td>
+                                  <td><?php echo $transaksigasonline->idTransaksi_Offline ?></td>
+                                  <td><?php echo $transaksigasonline->tanggalTransaksiOffline ?></td>
                                   <td><?php echo $transaksigasonline->idPangkalan ?></td>
                                   <td><?php echo $transaksigasonline->jumlahGas ?></td>
                                   <td><?php echo $transaksigasonline->tanggalpembelian ?></td>
@@ -109,6 +114,22 @@
                             </tr>
                           </tbody>
                             </table> 
+                             <table class="table table-striped ">
+                                        <thead>
+                                            <tr>
+                                                <th>Total :</th>
+                                                <td><?php if(empty($hasil)) {
+                                                echo "Data transaksi gas masih kosong";
+                                                }
+                                                else { ?>
+                                                  <?php foreach ($hasil as $transaksigasoffline) {?>
+                                                    <tr>
+                                                    <td><?php echo $transaksigasoffline->idTransaksi_Offline ?></td>
+                                               <?php } ?>
+                                               <?php } ?></td>
+                                                              </tr>
+                                                          </thead>
+                          </table>
                               
                        <!-- <div class="form-group">
                               <div class="col-lg-10 col-lg-offset-1">
