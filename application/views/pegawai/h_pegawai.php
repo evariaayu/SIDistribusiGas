3b3700ad2d3c1f90b26cc8eb1c336b5a07139dcc
pinasthikacintya <pinasthikacintya@gmail.com>
2015-03-16 09:52:19 +0700

    hm

---
+++ application/views/pegawai/h_pegawai.php
@@ -0,0 +1,9 @@
+<center>
+		<h1> <br> <br> </h1> <hr>
+		<img src="../assets/img/almas.png" alt="logo" width="500">
+		<hr>
+		<h1 font-size:3em>Selamat Datang</h1>
+
+		<p style="font-size:30px">PT. Almas Gasindo Jaya Abadi <br>
+			Jalan Kauman RT 03/ RW 01, Suruh, Kabupaten Semarang</p>
+	</center>
---
+++ application/views/pegawai/home_pegawai.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Title</title>
+</head>
+
+<body>
+	<center>
+		<h1>Selamat Datang</h1>
+		<p>This is a paragraph.</p>
+	</center>
+  
+  
+</body>
+
+</html>
