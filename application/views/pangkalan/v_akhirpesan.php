ff01df32477a2965beb9bfde546e93d421f2ddd8
Lubna Nur Aini <lubnaluben@gmail.com>
2015-03-24 07:16:39 +0700

    lala

---
+++ application/views/pangkalan/v_akhirpesan.php
@@ -0,0 +1,26 @@
+<h1><center>Kelola Pemesan Gas Online</center></h1>
+<br>
+
+<div class="col-md-2">
+  </div>
+  <div class="col-xs-1"></div>
+    <button type="button" class="btn btn-default btn-md btn-link">
+  <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> 
+  <?php echo anchor ('index.php/c_pesanonline','Pesan Gas') ?>
+</button>
+<br>
+<br>
+
+<div class="row">
+
+<div class="col-xs-2 col-md-offset-6">
+    <div class="input-group">
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Search</button>
+      </span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+<br>
+<br>
+<br>
