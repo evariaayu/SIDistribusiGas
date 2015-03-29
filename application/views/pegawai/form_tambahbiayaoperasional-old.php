7f48e3a91d93f19a3a483b2c53997a985a5ba410
evariaayu <evariaayu@gmail.com>
2015-03-29 08:21:31 +0700

    memperbaiki view

--- application/views/pegawai/form_tambahbiayaoperasional-old.php
+++ application/views/pegawai/form_tambahbiayaoperasional-old.php
@@ -1,69 +1,69 @@
-<h3><center>Form Tambah Biaya Operasional</center></h1>
-<br>
-<!--
-<div class="alert alert-warning alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <strong>Warning!</strong> <?php echo $error;?>
-</div>-->
-
-<div class="col-xs-offset-4 col-md-5">
+<div class="col-sm-offset-3 col-md-7 ">
+  <div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">
+    <h3 class="panel-title"><b><center>Form Tambah Biaya Operasional</center></b></h3>
+  </div>
+  <div class="panel-body">
 <form class="form-horizontal"  method="POST" action="<?php echo base_url() ?>index.php/kelola_operasional/do_upload" enctype="multipart/form-data" multiple="true">
 <?php echo $success;?>
   <div class="form-group">
     <!--pam-->
-    <label for="pengeluaranPAM" class="col-sm-2 control-label">PAM</label>
-    <div class="col-sm-10">
+    <label for="pengeluaranPAM" class="col-sm-3 control-label">PAM</label>
+    <div class="col-sm-6">
       <input type="text" class="form-control" id="pengeluaranPAM" name="pengeluaranPAM" placeholder="pam" required>
     </div>
   </div>
   <!-- input file pam-->
   <div class="form-group">
-    <label for="filePAM" class="col-sm-2 control-label">File PAM</label>
-    <div class="col-sm-10">
+    <label for="filePAM" class="col-sm-3 control-label">File PAM</label>
+    <div class="col-sm-6">
       <input type="file" name="filePAM">
-      <p class="help-block">file max. 2mb</p>
+      
     </div>
   </div>
 
 
   <!---pln-->
   <div class="form-group">
-    <label for="pengeluaranPLN" class="col-sm-2 control-label">PLN</label>
-    <div class="col-sm-10">
+    <label for="pengeluaranPLN" class="col-sm-3 control-label">PLN</label>
+    <div class="col-sm-6">
       <input id="pengeluaranPLN" name="pengeluaranPLN" class="form-control" placeholder="pln" required></input>
     </div>
   </div>
    <!-- input file pln-->
   <div class="form-group">
-    <label for="filePLN" class="col-sm-2 control-label">File PLN</label>
-    <div class="col-sm-10">
+    <label for="filePLN" class="col-sm-3 control-label">File PLN</label>
+    <div class="col-sm-6">
       <input type="file" name="filePLN">
-      <p class="help-block">file max. 2mb</p>
+      
     </div>
   </div>
   
   <!---internet-->
   <div class="form-group">
-    <label for="pengeluaranInternet" class="col-sm-2 control-label">Internet</label>
-    <div class="col-sm-10">
+    <label for="pengeluaranInternet" class="col-sm-3 control-label">Internet</label>
+    <div class="col-sm-6">
       <input id="pengeluaranInternet" name="pengeluaranInternet" class="form-control" placeholder="Internet" required></input>
     </div>
   </div>
    <!-- input file pln-->
   <div class="form-group">
-    <label for="fileInternet" class="col-sm-2 control-label">File Internet</label>
-    <div class="col-sm-10">
+    <label for="fileInternet" class="col-sm-3 control-label">File Internet</label>
+    <div class="col-sm-6">
       <input type="file" name="fileInternet">
-      <p class="help-block">file max. 2mb</p>
+     
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
+    <div class="col-sm-offset-3 col-sm-6">
       <button type="submit" class="btn btn-default">Simpan</button>
-      <!--<button type="submit" class="btn btn-default" href='pegawai/form_tambahbiayaoperasional'>Next</button>-->
+      <input class="btn btn-danger" type="reset" value="Cancel"><br>
     </div>
   </div>
   
 </form>
 
+</div>
+</div>
 </div>
--- application/views/pegawai/form_tambahbiayaoperasional.php
+++ application/views/pegawai/form_tambahbiayaoperasional.php
@@ -1,16 +1,16 @@
-<h3><center>Form Tambah Biaya Lain-lain</center></h1>
-<br>
-<?php echo $success;?>
-<div class="col-md-3">
-</div>
-
-<div class="col-md-6">
+<div class="col-md-6 col-sm-offset-3">
+  <div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">
+    <h3 class="panel-title"><b><center>Form Tambah Biaya Lain-lain</center></b></h3>
+  </div>
+  <div class="panel-body">
 <form class="form-horizontal"  method="POST" action="<?php echo base_url() ?>index.php/kelola_operasional/do_uploadlain" enctype="multipart/form-data">
-
+<?php echo $success;?>
 <!--- lainnya -->
   <div class="form-group">
     <label for="namabarang" class="col-sm-2 control-label">Nama Barang</label>
-    <div class="col-sm-10">
+    <div class="col-sm-6">
       <input id="namabarang" name="namabarang" class="form-control" placeholder="Nama Barang" ></input>
     </div>
   </div>
@@ -18,30 +18,30 @@
 <!--- harga -->
   <div class="form-group">
     <label for="harga" class="col-sm-2 control-label">Harga</label>
-    <div class="col-sm-10">
+    <div class="col-sm-6">
       <input id="harga" name="harga" class="form-control" placeholder="Harga" required></input>
     </div>
   </div>
 
   <!---Lampirkan File -->
  <div class="form-group">
-    <label for="filebarang" class="col-sm-2 control-label">File PAM</label>
-    <div class="col-sm-10">
+    <label for="filebarang" class="col-sm-2 control-label">File Barang</label>
+    <div class="col-sm-6">
       <input type="file" name="filebarang">
-      <p class="help-block">file max. 2mb</p>
-    </div>
-  </div>
-
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      
     </div>
   </div>
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default">Simpan</button>
+      <input class="btn btn-danger" type="reset" value="Cancel"><br>
     </div>
   </div>
+    </div>
+
+
   
 </form>
+
+</div>
+</div>
 </div>
