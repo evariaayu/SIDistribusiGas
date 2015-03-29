26209e10e7448bc42f20847e2686bc8992e07f4d
evariaayu <evariaayu@gmail.com>
2015-03-29 08:32:05 +0700

    update views/pegawai/form_pesanoffline.php

--- application/views/pegawai/form_pesanoffline.php
+++ application/views/pegawai/form_pesanoffline.php
@@ -1,15 +1,15 @@
-
-<h3><center>Form Pemesanan Gas</center></h1>
-  <br>
-  <div class="col-md-3">
+<div class="col-sm-offset-3 col-md-6 ">
+  <div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">
+    <h3 class="panel-title"><b><center>Form Pemesanan Gas</center></b></h3>
   </div>
-
-  <div class="col-md-6">
+  <div class="panel-body">
     <form class="form-horizontal"  method="POST" action="<?php echo base_url() ?>index.php/c_pesanoffline/pesan">
       <?php echo $success;?>
         <div class="form-group">
-          <label for="idPangkalan" class="col-sm-2 control-label">Pangkalan</label>
-          <div class="col-sm-10">
+          <label for="idPangkalan" class="col-sm-3 control-label">Pangkalan</label>
+          <div class="col-sm-7">
             <select class="form-control" name="idPangkalan" id="idPangkalan">
               <option value="0">--- Pilih Pangkalan ----</option>
            <?php foreach ($hasil as $row) {?>
@@ -23,8 +23,8 @@
 
       <div class="form-group">
         <!--jumlah barang rusak-->
-        <label for="hargagas" class="col-sm-2 control-label">Harga/1 gas</label>
-        <div class="col-sm-10">
+        <label for="hargagas" class="col-sm-3 control-label">Harga/1 gas</label>
+        <div class="col-sm-7">
           <div class="well well-sm"><?php echo $harga[0]['hargajual'] ?></div>
           <input type="hidden" id="harga" name="harga" class="form-control" value="<?php echo $harga[0]['hargajual'] ?>" readonly>
         </div>
@@ -32,8 +32,8 @@
 
       <!---jumlah barang kosong -->
       <div class="form-group">
-        <label for="jumlahGas" class="col-sm-2 control-label">Jumlah Order</label>
-        <div class="col-sm-10">
+        <label for="jumlahGas" class="col-sm-3 control-label">Jumlah Order</label>
+        <div class="col-sm-7">
           <input id="jumlahGas" name="jumlahGas" class="form-control" placeholder=" "></input>
         </div>
       </div>
@@ -44,10 +44,13 @@
 
 
   <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <input type="submit" name="submit" value="Submit">
+    <div class="col-sm-offset-3 col-sm-7">
+      <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+      <input class="btn btn-danger" type="reset" value="Cancel"><br>
     </div>
   </div>
 
   </form>
+</div>
+</div>
 </div>
