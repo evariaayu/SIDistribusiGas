94f9a4290fbb4a1892c925f6feaf61289f402c89
evariaayu <evariaayu@gmail.com>
2015-03-29 08:35:05 +0700

    update header ;)

--- application/views/header.php
+++ application/views/header.php
@@ -12,6 +12,8 @@
   
     <script type="text/javascript" src="<?php echo base_url('assets/js/date_time.js') ?>"></script>
     <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('dist/css/flat-ui.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('docs/assets/css/demo.css') ?>" rel="stylesheet">
     <!--
     <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
     
@@ -28,8 +30,7 @@
 
 
 
-<link href="<?php echo base_url('dist/css/flat-ui.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('docs/assets/css/demo.css') ?>" rel="stylesheet">
+
 
     -->
 
